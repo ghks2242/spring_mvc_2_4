43918ff637cd3f16054c5c333fde2c67034b6deb
yunchanghwan <ghks2242@naver.com>
2026-05-14 22:30:26 +0900

    76장부터시작

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,60 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+//    @GetMapping("/")
     public String home() {
-        return "redirect:/items";
+        return "home";
+    }
+
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name="memberId", required = false) Long memberId, Model model) {
+        if (memberId == null) {
+            return "home";
+        }
+
+        // 로그인
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @PostMapping("/logout")
+    public String logout(@CookieValue(name="memberId") Long memberId, HttpServletResponse response) {
+        expireCookie(memberId, response);
+        return "redirect:/";
+    }
+
+    private static void expireCookie(Long memberId, HttpServletResponse response) {
+        if(memberId != null){
+            Cookie idCookie = new Cookie("memberId", null);
+            idCookie.setMaxAge(0);
+            response.addCookie(idCookie);
+        }
     }
 }
